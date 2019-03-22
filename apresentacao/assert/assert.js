f1f33255c649ca965555c37bfe35fe92b64e89f7
João Vitor Zavatin <32987317+JVZavatin@users.noreply.github.com>
2019-03-22 09:35:54 -0300

    Update assert.js
    Adicionei meus exemplos sobre o chai ao arquivo da apresentação

--- apresentacao/assert/assert.js
+++ apresentacao/assert/assert.js
@@ -23,3 +23,33 @@ function fun() {
 assert.equal(fun(), 8, "Verificando string com número");
 assert.deepStrictEqual(fun(), 8, "Verificando string com número");
 */
+/*  CHAI
+
+$ npm install chai   // Instala a biblioteca de asserts Chai
+
+// 3 tipos diferentes de interfaces das funcoes do chai
+chai.should();  //Primeira interface
+
+foo.should.be.a('string');
+foo.should.equal('bar');
+foo.should.have.lengthOf(3);
+tea.should.have.property('flavors')
+  .with.lengthOf(3);
+  
+var expect = chai.expect;  //Segunda interface
+
+expect(foo).to.be.a('string');
+expect(foo).to.equal('bar');
+expect(foo).to.have.lengthOf(3);
+expect(tea).to.have.property('flavors')
+  .with.lengthOf(3);
+  
+var assert = chai.assert; // Terceira Interface
+
+assert.typeOf(foo, 'string');
+assert.equal(foo, 'bar');
+assert.lengthOf(foo, 3)
+assert.property(tea, 'flavors');
+assert.lengthOf(tea.flavors, 3);
+
+*/
