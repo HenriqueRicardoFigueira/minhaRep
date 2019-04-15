2334d432b49aa4e108b117d7d8fe01545f43728c
unknown <henriquefigueira@alunos.utfpr.edu.br>
2019-04-15 20:40:34 -0300

    Correct bug on firebase auth facebook

--- src/components/facebookLogin.js
+++ src/components/facebookLogin.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
+import {TouchableOpacity , Text} from 'react-native';
+import FBSDK, {GraphRequest} from 'react-native-fbsdk';
+import {styles} from './styles';
+import { withNavigation } from 'react-navigation';
+import firebase from 'react-native-firebase';
 
-import { Button, StyleSheet } from 'react-native';
 
-// import { Container } from './styles';
+class FacebookLogin extends Component {
 
-export default class FacebookLogin extends Component {
+  face = async () => (
+    FBSDK.LoginManager.logInWithReadPermissions(['public_profile', 'email']).then(
+      function (result) {
+        if (result.isCancelled) {
+          alert('Login was cancelled');
+        } else {
+          alert('Login was successful with permissions: '
+            + result.grantedPermissions.toString());
+        }
+        
+        FBSDK.AccessToken.getCurrentAccessToken()
+        .then(
+          (data) => {
+            /*let req = new GraphRequest('/me', {
+              httpMethod: 'GET',
+              version: 'v2.5',
+              parameters: {
+                  'fields': {
+                      'string' : 'email,name'
+                  }
+              }
+              }, (err, res) => {
+                console.log(err, res);
+              });
+              console.log(req.parameters.fields);
+              */
+              const credential = firebase.auth.FacebookAuthProvider.credential(data.accessToken);
+              firebase.auth().signInWithCredential(credential);
+              console.log(firebase.auth().currentUser);
+        }); 
+      },
+      function(error) {
+        console.log('Login failed with error: ' + error);
+      }
+    )
+  );
+
   render() {
-    return <Button style={styles.button} title="Continue with Facebook"/>;
+    return (
+      <TouchableOpacity style={styles.button} onPress={this.face}>
+        <Text style={styles.butonText}> Continue with Facebook </Text>
+      </TouchableOpacity>
+    );
   }
 }
 
-
-const styles = StyleSheet.create({
-    button: {
-        height: 45,
-        alignSelf: 'stretch',
-        paddingHorizontal: 20,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-})
+export default withNavigation(FacebookLogin);
