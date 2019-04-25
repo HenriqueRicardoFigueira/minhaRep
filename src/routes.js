7ed7b96c140d90e33498e6ffb73f5f0aa99bc808
Daniel Venturini <danielventurini021@gmail.com>
2019-04-25 10:44:02 -0300

    Update routes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,19 @@
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import Home from './pages/home';
-import LoginPage from './pages/login';
+import Login from './pages/login';
 import RepForm from './pages/repForm';
 import RepCRUD from './pages/repCRUD';
 import RepEdit from './pages/repEdit';
+import RepCard from './pages/repCard';
 
 const Routes = createAppContainer(
     createSwitchNavigator({
-        Login: LoginPage,
+        Login: Login,
         Home: Home,
         RepCRUD: RepCRUD,
         RepEdit: RepEdit,
         RepForm: RepForm,
+        RepCard: RepCard
     }, {
         initialRouteName: 'Login'
     })
