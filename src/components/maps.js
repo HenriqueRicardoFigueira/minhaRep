1929c3bb3c5fa17c230634e52d10140e49c31140
henrislip <henriquefigueria@alunos.utfpr.edu.br>
2019-04-24 22:36:32 -0300

    Solved navigation with facebook auth, added maps.js, solved problems with google-json

---
+++ src/components/maps.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { styles } from './styles';
+import { withNavigation } from 'react-navigation'
+import {View , Text} from 'react-native';
+
+class Maps extends Component {
+  render() {
+    return <View/>
+  }
+}
+export default withNavigation(Maps);
