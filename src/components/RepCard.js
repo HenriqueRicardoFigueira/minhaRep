09c3bb822f93907884ca4b09484aeb6803f39ac2
Daniel Venturini <danielventurini021@gmail.com>
2019-04-29 13:19:24 -0300

    Fix some styles in RepCard

--- src/components/RepCard.js
+++ src/components/RepCard.js
@@ -30,11 +30,21 @@ export default class RepCard extends Component {
             <View>
               <Text style={styleSwipe.repTitle}>{this.state.repTitle}</Text>
               <View style={styleSwipe.iconViewTop}>
-                <Icon size={30} name='snowflake-o' color='#000' style={styleSwipe.iconTop} />
-                <Icon size={30} name='wheelchair-alt' color='#000' style={styleSwipe.iconTop} />
-                <Icon size={30} name='bicycle' color='#000' style={styleSwipe.iconTop} />
-                <Icon size={30} name='youtube' color='#000' style={styleSwipe.iconTop} />
-                <Icon size={30} name='steam' color='#000' style={styleSwipe.iconTop} />
+                <View style={styleSwipe.iconTop}>
+                  <Icon size={30} name='snowflake-o' color='#000' />
+                </View>
+                <View style={styleSwipe.iconTop}>
+                  <Icon size={30} name='wheelchair-alt' color='#000'/>
+                </View>
+                <View style={styleSwipe.iconTop}>
+                  <Icon size={30} name='bicycle' color='#000'/>
+                </View>
+                <View style={styleSwipe.iconTop}>
+                  <Icon size={30} name='youtube' color='#000'/>
+                </View>
+                <View style={styleSwipe.iconTop}>
+                  <Icon size={30} name='steam' color='#000'/>
+                </View>
               </View>
             </View>
           </View>
@@ -47,20 +57,20 @@ export default class RepCard extends Component {
           {/* TERCEIRA VIEW */}
           <View>
             <View style={styleSwipe.iconViewButton}>
-              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{text: 'OK'}])} >
+              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{ text: 'OK' }])} >
                 <Icon name='bathtub' size={35} color='#000' />
               </View>
-              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{text: 'OK'}])} >
-                <Icon name='bed' size={35} color='#000' onPress={() => Alert.alert('', 'Quantidade de Camas', [{text: 'OK'}])} />
+              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{ text: 'OK' }])} >
+                <Icon name='bed' size={35} color='#000' onPress={() => Alert.alert('', 'Quantidade de Camas', [{ text: 'OK' }])} />
               </View>
-              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{text: 'OK'}])} >
-                <Icon name='users' size={35} color='#000' onPress={() => Alert.alert('', 'Quantidade de Membros', [{text: 'OK'}])} />
+              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{ text: 'OK' }])} >
+                <Icon name='users' size={35} color='#000' onPress={() => Alert.alert('', 'Quantidade de Membros', [{ text: 'OK' }])} />
               </View>
-              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{text: 'OK'}])} >
-                <Icon name='money' size={35} color='#000' onPress={() => Alert.alert('', 'Preço do Aluguel', [{text: 'OK'}])} />
+              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{ text: 'OK' }])} >
+                <Icon name='money' size={35} color='#000' onPress={() => Alert.alert('', 'Preço do Aluguel', [{ text: 'OK' }])} />
               </View>
-              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{text: 'OK'}])} >
-                <Icon name='map-o' size={35} color='#000' onPress={() => Alert.alert('', 'Mapa da República', [{text: 'OK'}])} />
+              <View style={styleSwipe.iconButton} onPress={() => Alert.alert('', 'Quantidade de Banheiros', [{ text: 'OK' }])} >
+                <Icon name='map-o' size={35} color='#000' onPress={() => Alert.alert('', 'Mapa da República', [{ text: 'OK' }])} />
               </View>
             </View>
           </View>
@@ -91,11 +101,11 @@ const styleSwipe = StyleSheet.create({
     flexDirection: 'row',
   },
   iconPhoto: {
-    width: styles.screen.width*0.25,
-    height: styles.screen.width*0.25,
     borderRadius: 10,
     resizeMode: 'cover',
     backgroundColor: '#ffb380',
+    width: styles.screen.width * 0.25,
+    height: styles.screen.width * 0.25,
   },
   repTitle: {
     width: 190,
@@ -105,37 +115,43 @@ const styleSwipe = StyleSheet.create({
     flexDirection: 'column',
   },
   iconViewTop: {
+    padding: 10,
     flexDirection: 'row',
-    paddingRight: 10,
     justifyContent: 'space-between',
-    width: styles.screen.width*0.5,
+    width: styles.screen.width * 0.5,
   },
   iconTop: {
     margin: 2,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: '#ff944d',
+    width: styles.screen.width*0.11,
+    height: styles.screen.width*0.11,
   },
   iconViewButton: {
     padding: 10,
     flexDirection: 'row',
     alignItems: 'stretch',
     justifyContent: 'space-between',
-    width: styles.screen.width*0.85,
+    width: styles.screen.width * 0.85,
   },
   iconButton: {
+    margin: 1.8,
     borderRadius: 100,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#ff944d',
-    width: styles.screen.width*0.17,
-    height: styles.screen.width*0.17,
+    width: styles.screen.width * 0.17,
+    height: styles.screen.width * 0.17,
   },
   repImage: {
     borderWidth: 2,
     borderRadius: 10,
     alignSelf: 'center',
     borderColor: '#8c8c8c',
-    width: styles.card.width*0.95,
-    height: styles.card.height*0.6,
+    width: styles.card.width * 0.95,
+    height: styles.card.height * 0.6,
   }
 });
 
