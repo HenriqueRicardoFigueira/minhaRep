dc3ec404e3889b5f8d1272e36657bf16484efbdb
Daniel Venturini <danielventurini021@gmail.com>
2019-05-04 17:41:21 -0300

    Get localization from phone

--- src/components/maps.js
+++ src/components/maps.js
@@ -9,6 +9,7 @@ class Maps extends Component {
     super(props);
 
     this.state = {
+      region: null,
       latitude: props.latitude,
       longitude: props.longitude,
       latitudeDelta: props.latitudeDelta,
@@ -16,15 +17,53 @@ class Maps extends Component {
     }
   }
 
-  render() {
-    return (
-      <MapView style={styles.map}
-        initialRegion={{
+  async componentDidMount() {
+
+    // se a latitude já foi passado pelo props
+    if (this.state.latitude != undefined && this.state.longitude != undefined) {
+      // cria o objeto region com a lat e long que foi passado
+      this.setState({
+        region: {
           latitude: this.state.latitude,
           longitude: this.state.longitude,
           latitudeDelta: this.state.latitudeDelta,
           longitudeDelta: this.state.longitudeDelta,
-        }}
+        }
+      })
+
+      return
+    }
+
+    // recupera a localização do celular
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => { // função de sucesso
+        this.setState({
+          region: {
+            latitude,
+            longitude,
+            latitudeDelta: this.state.latitudeDelta,
+            longitudeDelta: this.state.longitudeDelta,
+          }
+        })
+      },
+      () => { // função de erro
+      },
+      {
+        timeout: 2000,
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+      }
+    )
+  }
+
+  render() {
+    const { region } = this.state;
+
+    return (
+      <MapView style={styles.map}
+        initialRegion={region}
+        showsUserLocation
+        loadingEnabled
       />
     );
   }
@@ -33,10 +72,8 @@ class Maps extends Component {
 // propriedades padrão do componente
 // mapa sobre o Brasil
 Maps.defaultProps = {
-  latitude: -15.11455287,
-  longitude: -49.48242188,
-  latitudeDelta: 45,
-  longitudeDelta: 45,
+  latitudeDelta: 0.0143,
+  longitudeDelta: 0.0134,
 }
 
 export default withNavigation(Maps);
