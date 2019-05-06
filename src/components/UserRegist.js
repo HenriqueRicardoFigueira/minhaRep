afca0e9c0e3b7e9b2f5e87f4c0e263ec3dedb317
Daniel Venturini <danielventurini021@gmail.com>
2019-05-05 23:29:52 -0300

    Add fields verifications

--- src/components/UserRegist.js
+++ src/components/UserRegist.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Button, Item, Input, Label } from 'native-base';
-import { View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import firebase from 'react-native-firebase';
-import {styles} from './styles';
+import { styles } from './styles';
 
 import { withNavigation } from 'react-navigation';
 
@@ -27,11 +27,19 @@ class UserRegist extends Component {
       age: '',
       bio: '',
       uid: '',
+      borderColorAge: '#e6e6e6',
+      borderColorName: '#e6e6e6',
+      borderColorEmail: '#e6e6e6',
+      borderColorPassword: '#e6e6e6',
     };
   };
 
   registerUser = async () => {
     const { email, age, name, password } = this.state;
+    if(!this.canRegister(email, age, name, password)) {
+      return
+    }
+
     //try {
     // REGISTRA O USUARIO NO AUTHENTICATION
     // RETORNA UM OBJETO DO TIPO user
@@ -59,6 +67,7 @@ class UserRegist extends Component {
       bio: '',
       isSubmited: true,
     });
+
     this.props.navigation.navigate("Home");
   }
 
@@ -78,37 +87,47 @@ class UserRegist extends Component {
 
         <Text h1>Tela de registro </Text>
 
-        <Item floatingLabel style={styles.floatInput}>
+        <Item floatingLabel style={styles.floatInput}
+          style={{ borderColor: this.state.borderColorName }}>
           <Label>Digite seu nome:</Label>
           <Input
             value={this.state.name}
             onChangeText={(name) => this.setState({ name })}
+            onEndEditing={() => this.nameColor(this.state.name)}
           ></Input>
         </Item>
 
-        <Item floatingLabel style={styles.floatInput}>
+        <Item floatingLabel style={styles.floatInput}
+          style={{ borderColor: this.state.borderColorEmail }}>
           <Label>Digite seu email:</Label>
           <Input
             value={this.state.email}
+            autoCapitalize={'none'}
             onChangeText={(email) => this.setState({ email })}
+            onEndEditing={() => this.emailColor(this.state.email)}
           ></Input>
         </Item>
 
-        <Item floatingLabel style={styles.floatInput}>
+        <Item floatingLabel style={styles.floatInput}
+          style={{ borderColor: this.state.borderColorPassword }}>
           <Label>Digite sua senha:</Label>
           <Input
             value={this.state.password}
             secureTextEntry={true}
+            autoCapitalize={'none'}
             onChangeText={(password) => this.setState({ password })}
+            onEndEditing={() => this.passwordColor(this.state.password)}
           ></Input>
         </Item>
 
-        <Item floatingLabel style={styles.floatInput}>
+        <Item floatingLabel style={styles.floatInput}
+        style={{ borderColor: this.state.borderColorAge }}>
           <Label>Digite sua idade:</Label>
           <Input
             value={this.state.age}
             keyboardType='number-pad'
             onChangeText={(age) => this.setState({ age })}
+            onEndEditing={() => this.ageColor(this.state.age)}
           ></Input>
         </Item>
 
@@ -118,6 +137,68 @@ class UserRegist extends Component {
 
       </View>
     );
+  }
+
+  color = (field, regex) => {
+    var newColor = null
+    if (regex.test(String(field).toLowerCase())) {
+      newColor = '#e6e6e6'
+    } else {
+      newColor = '#ff0000'
+    }
+
+    return newColor
+  }
+
+  nameColor = (name) => {
+    newColor = this.color(name, /^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\d.-]{0,19}$/)
+
+    this.setState({
+      borderColorName: newColor,
+    })
+
+    return newColor == '#e6e6e6'
+  }
+
+  emailColor = (email) => {
+    newColor = this.color(email, /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+
+    this.setState({
+      borderColorEmail: newColor,
+    })
+
+    return newColor == '#e6e6e6'
+  }
+
+  passwordColor = (password) => {
+    newColor = this.color(password, /^([a-zA-Z0-9@*#]{4,15})$/)
+
+    this.setState({
+      borderColorPassword: newColor,
+    })
+
+    return newColor == '#e6e6e6'
+  }
+
+  ageColor = (age) => {
+    newColor = this.color(age, /^[0-9][0-9]$/)
+
+    this.setState({
+      borderColorAge: newColor
+    })
+
+    return newColor == '#e6e6e6'
+  }
+
+  canRegister = (email, age, name, password) => {
+    // se fizer as chamadas de função no retorno
+    // só vai alterar a cor do primeiro que estiver fora do padrão
+    boolAge = this.ageColor(age)
+    boolName = this.nameColor(name)
+    boolEmail = this.emailColor(email)
+    boolPassword = this.passwordColor(password)
+
+    return boolAge && boolName && boolEmail && boolPassword
   }
 }
 
