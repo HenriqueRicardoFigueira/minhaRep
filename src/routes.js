50243b1eca9a200ababffb2ce61b9eb5c6684b8b
Daniel Venturini <danielventurini021@gmail.com>
2019-05-29 17:10:53 -0300

    Removed not necessary files

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,6 @@ import HomeRep from './pages/home/homeRep';
 import Login from './pages/login';
 import RepForm from './pages/repForm';
 import RepCRUD from './pages/repCRUD';
-import RepEdit from './pages/repEdit';
 import Maps from './pages/maps';
 import RepCard from './pages/repCard';
 import UserRegist from './pages/userRegist';
@@ -25,7 +24,6 @@ const Routes = createAppContainer(
     createSwitchNavigator({
         Login: Login,
         Home: Teste,
-        RepEdit: RepEdit,
         RepForm: RepForm,
         RepCRUD: RepCRUD,
         RepCard: RepCard,
