6492c8157a221c1e024f022afb7a80eb9d5e293c
Daniel Venturini <danielventurini021@gmail.com>
2019-06-07 15:06:22 -0300

    Add gallery in RepCRUD. #39

--- src/components/RepCRUD.js
+++ src/components/RepCRUD.js
@@ -5,6 +5,7 @@ import { styles } from './styles';
 import { Button, Item, Input, Label } from 'native-base';
 import { withNavigation } from 'react-navigation';
 import { imageSelect } from './commonPhoto'
+import Gallery from 'rep-gallery';
 
 import axios from 'axios';
 import { nameColor, memberColor, bioColor, cepColor, genericColor } from '../formValidation'
@@ -211,12 +212,6 @@ class RepCRUD extends Component {
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.container}>
 
-          <Image
-            style={{ width: 100, height: 100 }}
-            disabled={!this.state.gotUrl}
-            source={{ uri: this.state.photoURL[0] }}  // apenas para não gerar erro.
-          />
-
           <Item floatingLabel style={Object.assign({ borderColor: this.state.borderColorName }, styles.floatInput)}>
             <Label>Nome da Republica:</Label>
             <Input
@@ -301,8 +296,8 @@ class RepCRUD extends Component {
             <Text style={styles.buttonText}> Lista de Membros </Text>
           </Button>
 
-          <Button style={styles.button} onPress={() => {imageSelect(this.state.admUID, this.updateStateCallback)}}>
-            <Text style={styles.buttonText}> Enviar Foto </Text>
+          <Button style={styles.button} onPress={() => this.props.navigation.navigate("PhotoGallery", {photoURL: this.state.photoURL})} >
+            <Text style={styles.buttonText}> Fotos </Text>
           </Button>
 
           <Button style={styles.button} onPress={this.editRep}>
